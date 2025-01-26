f9c48e6841a490ebc392361f2968c3ed78601aa0
MahmoudAshraf2899 <mahmodashrf79@gmail.com>
2025-01-26 14:22:32 +0200

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,130 @@
 import Image from "next/image";
+import logo from "../../public/icons/PTCLOGO.png";
+import hero from "../../public/icons/hero.jpg";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <section>
+      <div className="min-h-screen bg-cover bg-center">
+        {/* Navigation */}
+        <nav className="flex justify-between items-center pl-24 pr-28 px-6 py-4 bg-opacity-60">
+          {/* Logo */}
+          <div className="flex items-center space-x-2">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src={logo}
+              alt="Logo"
+              width={120}
+              height={100}
+              className="rounded-full"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+          <div className="flex flex-row gap-8">
+            {/* Navigation Links */}
+            <div className="md:flex navFont text-[16px] hover:text-slate-100 active:text-slate-100">
+              <Link href="/">Home</Link>
+            </div>
+
+            <div className="md:flex navFont text-[16px] hover:text-slate-100 active:text-slate-100">
+              <Link href="/Developments">Developments</Link>
+            </div>
+
+            <div className="md:flex navFont text-[16px] hover:text-slate-100 active:text-slate-100">
+              <Link href="/Constructions">Constructions</Link>
+            </div>
+
+            <div className="md:flex navFont text-[16px] hover:text-slate-100 active:text-slate-100">
+              <Link href="/Home">About Us</Link>
+            </div>
+            <div className="md:flex navFont text-[16px] hover:text-slate-100 active:text-slate-100">
+              <Link href="/Home">Contact Us</Link>
+            </div>
+          </div>
+          {/* Buttons */}
+          <div className="flex space-x-4">
+            <button className="px-4 py-2 border border-slate-100  flex item-center gap-2 rounded-lg shadow-lg">
+              <svg
+                width="25px"
+                height="25px"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  {" "}
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M11.0748 7.50835C9.74622 6.72395 8.25 7.79065 8.25 9.21316V14.7868C8.25 16.2093 9.74622 17.276 11.0748 16.4916L15.795 13.7048C17.0683 12.953 17.0683 11.047 15.795 10.2952L11.0748 7.50835ZM9.75 9.21316C9.75 9.01468 9.84615 8.87585 9.95947 8.80498C10.0691 8.73641 10.1919 8.72898 10.3122 8.80003L15.0324 11.5869C15.165 11.6652 15.25 11.8148 15.25 12C15.25 12.1852 15.165 12.3348 15.0324 12.4131L10.3122 15.2C10.1919 15.271 10.0691 15.2636 9.95947 15.195C9.84615 15.1242 9.75 14.9853 9.75 14.7868V9.21316Z"
+                    fill="#eee"
+                  ></path>{" "}
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M12 1.25C6.06294 1.25 1.25 6.06294 1.25 12C1.25 17.9371 6.06294 22.75 12 22.75C17.9371 22.75 22.75 17.9371 22.75 12C22.75 6.06294 17.9371 1.25 12 1.25ZM2.75 12C2.75 6.89137 6.89137 2.75 12 2.75C17.1086 2.75 21.25 6.89137 21.25 12C21.25 17.1086 17.1086 21.25 12 21.25C6.89137 21.25 2.75 17.1086 2.75 12Z"
+                    fill="#EEE"
+                  ></path>{" "}
+                </g>
+              </svg>
+              <span>Introduction</span>
+            </button>
+            <button className="px-4 py-2  mainBackground  rounded-lg shadow-lg">
+              Enquire
+            </button>
+          </div>
+        </nav>
+
+        {/* Main Content */}
+        <div className="flex items-center justify-center p-28 h-[calc(100vh-80px)]">
+          <Image
+            src={hero}
+            alt="MainHeroImage"
+            width={700}
+            height={800}
+            className="rounded-lg"
+          />
+          <div className="flex flex-col justify-start items-start">
+            <h1 className="text-5xl md:text-6xl font-bold text-white uppercase text-center mb-4">
+              Best Solution For Your Business
+            </h1>
+
+            <div className="flex flex-row w-full gap-4 pl-14">
+              <button className="px-8 py-4 mainBackground rounded-lg shadow-lg w-full">
+                Developments
+              </button>
+              <button className="px-8 py-4 mainBackground rounded-lg shadow-lg w-full">
+                Constructions
+              </button>
+            </div>
+            {/* Sub Image Section */}
+            <div className="flex justify-start items-center">
+              <div className="flex flex-row justify-center items-center bg-slate-800 rounded-full">
+                <Image
+                  src={hero}
+                  alt="SubHeroImage"
+                  width={70}
+                  height={120}
+                  className="rounded-full"
+                />
+                <div className="flex flex-col justify-center items-center">
+                  <h3 className="mb-3">Smarter Property Deals</h3>
+                  <p>
+                    Trusted by professionals for buying, selling and invest in
+                    real estate with advanced tools, market, insights and expert
+                    guidence.{" "}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </section>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,20 @@
-import type { Config } from "tailwindcss";
+import rippleui from 'rippleui';
 
-export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  plugins: [rippleui],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    fontFamily: {
+      satoshi: ['Satoshi', 'sans-serif'],
     },
+    screens: {
+      '2xsm': '375px',
+      xsm: '425px',
+      '3xl': '2000px',
+      
+    },
+     
   },
-  plugins: [],
-} satisfies Config;
+};
