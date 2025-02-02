ba03a62c3912429231f779e9ca45c2ae23403322
MahmoudAshraf2899 <mahmodashrf79@gmail.com>
2025-02-03 00:16:02 +0200

    finish working on constructions , developments projects integration with back end

--- src/components/Constructions/Constructions.tsx
+++ src/components/Constructions/Constructions.tsx
@@ -1,59 +1,141 @@
+"use client";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import subHeroImage from "../../../public/icons/SubHeroMain.jpg";
 import project from "../../../public/icons/Contruction1.jpg";
 import Link from "next/link";
+const BASE_URL = "https://ptcbackend-001-site1.jtempurl.com";
+interface Project {
+  id: string;
+  title: string;
+  description: string;
+  image: string;
+}
 const Constructions = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [constructionData, setConstructionData] = useState<{
+    mainImage: string;
+    title: string;
+  } | null>(null);
+  const [projectData, setProjectsData] = useState<
+    {
+      id: string;
+      title: string;
+      description: string;
+      image: string;
+    }[]
+  >([]);
+  useEffect(() => {
+    // Fetch data from API
+    setIsLoading(true);
+    const fetchProjectData = async () => {
+      try {
+        const mainDataResponse = await fetch(`${BASE_URL}/api/Constructions/1`);
+        if (!mainDataResponse.ok) throw new Error("Failed to fetch hero data");
+
+        const mainData = await mainDataResponse.json();
+        setConstructionData({
+          ...mainData,
+          mainImage: BASE_URL + mainData.data.mainImage,
+          title: mainData.data.title,
+        });
+        const response = await fetch(`${BASE_URL}/api/Constructions`);
+        if (!response.ok) throw new Error("Failed to fetch hero data");
+
+        const result = await response.json();
+        if (Array.isArray(result.data)) {
+          setProjectsData(
+            result.data.map((project: Project) => ({
+              id: project.id,
+              title: project.title,
+              description: project.description,
+              image: BASE_URL + project.image,
+            }))
+          );
+        } else {
+          console.error("Invalid response format: expected an array in 'data'");
+        }
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error fetching hero data:", error);
+      }
+    };
+
+    fetchProjectData();
+  }, []);
   return (
     <>
       <div className="relative h-screen">
         {/* Background Image */}
         <Image
-          src={subHeroImage}
+          src={
+            constructionData?.mainImage
+              ? constructionData.mainImage
+              : subHeroImage
+          }
           alt="Hero Background"
           layout="fill"
           objectFit="cover"
           className="absolute opacity-55 inset-0"
+          loading="eager"
         />
 
         <div className="relative z-10 flex flex-col items-center justify-center h-full px-4 sm:px-8 md:px-16">
           <h2 className="font-bold text-2xl sm:text-3xl lg:text-4xl text-white CalistogaFont text-center">
-            Each unit is designed to be a unique residential and investment
-            opportunity.
+            {constructionData?.title
+              ? constructionData.title
+              : "Specialists in Residential, Commercial, and Investment Properties"}
           </h2>
           <h1 className="mt-4 CalistogaFont text-4xl sm:text-6xl lg:text-8xl font-extrabold text-white text-center">
             Our Constructions
           </h1>
         </div>
       </div>
+      {/* Project Section */}
+      {projectData.map((project, index) => (
+        <div key={project.id}>
+          <div
+            className={`flex flex-col sm:flex-row justify-center items-center w-full lg:mt-20 md:mt-10 sm:mt-10 xs:mt-10 px-4 sm:px-8 
+                  ${index % 2 === 0 ? "sm:flex-row" : "sm:flex-row-reverse"}`}
+          >
+            {/* Text Section */}
+            <div className="flex flex-col w-full sm:w-1/3 pr-4 sm:pl-10 lg:pr-28">
+              <h2 className="text-4xl sm:text-4xl CalistogaFont font-black text-white uppercase">
+                <Link href={`/Constructions/1`} className="cursor-pointer">
+                  {project.title}
+                </Link>
+              </h2>
+              <p className="leading-7 font-bold text-white interFont text-sm sm:text-base lg:text-[14px]">
+                {project.description}
+              </p>
+            </div>
 
-      <div className="flex flex-col sm:flex-row justify-center items-center w-full mt-20 px-4 sm:px-8">
-        {/* Text Section */}
-        <div className="flex flex-col w-full sm:w-1/3 pl-4 sm:pl-10 lg:pl-28">
-          <h2 className="text-4xl sm:text-4xl CalistogaFont font-black text-white uppercase">
-            <Link href={`/Constructions/1`} className="cursor-pointer">
-              THE LATIN QUARTER IN NEW ALAMEIN
-            </Link>
-          </h2>
-          <p className="leading-7 font-bold text-white interFont text-sm sm:text-base lg:text[14px]">
-            The company successfully executed a significant number of
-            distinguished buildings in this prominent project in collaboration
-            with Gamma Company and the New Urban Communities Auth... See More.
-          </p>
+            {/* Image Section */}
+            <div
+              className={`w-full sm:w-2/3 flex flex-col               
+              ${
+                index % 2 === 0
+                  ? "items-center sm:items-end pr-4"
+                  : "items-start sm:items-start pl-4"
+              }   sm:pr-10 mt-8 sm:mt-0`}
+            >
+              <Image
+                src={project.image}
+                alt="project"
+                className="rounded-lg w-full sm:w-[540px] sm:h-[290px] object-cover"
+                width={540}
+                height={290}
+                loading="eager"
+              />
+            </div>
+          </div>
+
+          {/* Divider */}
+          <div className="z-10 flex flex-col items-center justify-center h-full mt-16">
+            <div className="divider w-1/2 sm:w-3/4 lg:w-1/2"></div>
+          </div>
         </div>
-
-        {/* Image Section */}
-        <div className="w-full sm:w-2/3 flex flex-col items-center sm:items-end pr-4 sm:pr-10 mt-8 sm:mt-0">
-          <Image
-            src={project}
-            alt="project"
-            className="rounded-lg w-full sm:w-[540px] sm:h-[290px] object-cover"
-          />
-        </div>
-      </div>
-
-      <div className="z-10 flex flex-col items-center justify-center h-full mt-16">
-        <div className="divider w-1/2 sm:w-3/4 lg:w-1/2"></div>
-      </div>
+      ))}
     </>
   );
 };
--- src/components/Home/InterestSection.tsx
+++ src/components/Home/InterestSection.tsx
@@ -1,85 +1,134 @@
+"use client";
+import { useEffect, useState } from "react";
 import subHeroImage from "../../../public/icons/hero.jpg";
 import Image from "next/image";
+import Loader from "../Loader/loader";
 
+const BASE_URL = "https://ptcbackend-001-site1.jtempurl.com";
+interface Project {
+  id: string;
+  title: string;
+}
 const InterestSection = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [projectData, setProjectsData] = useState<
+    {
+      id: string;
+      title: string;
+      description: string;
+      image: string;
+    }[]
+  >([]);
+  useEffect(() => {
+    // Fetch data from API
+    setIsLoading(true);
+    const fetchProjectData = async () => {
+      try {
+        const response = await fetch(`${BASE_URL}/api/Lookup/projects`);
+        if (!response.ok) throw new Error("Failed to fetch hero data");
+
+        const result = await response.json();
+        if (Array.isArray(result.data)) {
+          setProjectsData(
+            result.data.map((project: Project) => ({
+              id: project.id,
+              title: project.title,
+            }))
+          );
+        } else {
+          console.error("Invalid response format: expected an array in 'data'");
+        }
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error fetching hero data:", error);
+      }
+    };
+
+    fetchProjectData();
+  }, []);
   return (
-    <div className="relative h-screen bg-gray-800 mt-28">
-      {/* Background Image */}
-      <Image
-        src={subHeroImage}
-        alt="Hero Background"
-        layout="fill"
-        objectFit="cover"
-        className="absolute inset-0"
-      />
+    <>
+      {isLoading ? <Loader /> : null}
+      <div className="relative h-screen bg-gray-800 mt-28">
+        {/* Background Image */}
+        <Image
+          src={subHeroImage}
+          alt="Hero Background"
+          layout="fill"
+          objectFit="cover"
+          className="absolute inset-0"
+        />
 
-      {/* Overlay */}
-      <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+        {/* Overlay */}
+        <div className="absolute inset-0 bg-black bg-opacity-50"></div>
 
-      {/* Form Container */}
-      <div className="relative z-10 flex items-center justify-center h-full">
-        <form className="bg-white p-8 px-14  rounded-2xl shadow-lg w-full max-w-md">
-          <h2 className="text-4xl CalistogaFont font-black tracking-tight text-gray-800 mb-6 ">
-            Register your interest
-          </h2>
-          <span className="interFont text-center  text-black text-[14px]">
-            Interested in finding out more about our projects? Fill out the form
-            below and a member of our sales team will contact you shortly.
-          </span>
+        {/* Form Container */}
+        <div className="relative z-10 flex items-center justify-center h-full">
+          <form className="bg-white p-8 px-14  rounded-2xl shadow-lg w-full max-w-md">
+            <h2 className="text-4xl CalistogaFont font-black tracking-tight text-gray-800 mb-6 ">
+              Register your interest
+            </h2>
+            <span className="interFont text-center  text-black text-[14px]">
+              Interested in finding out more about our projects? Fill out the
+              form below and a member of our sales team will contact you
+              shortly.
+            </span>
 
-          <div className="mb-4 mt-4">
-            <input
-              type="text"
-              id="firstName"
-              name="Full Name"
-              className="w-full p-3 interFont text-[14px] text-slate-500 interestSectionBorder bg-slate-300 rounded-xl   focus:outline-none focus:ring-2 focus:ring-slate-300"
-              placeholder="Full Name"
-            />
-          </div>
+            <div className="mb-4 mt-4">
+              <input
+                type="text"
+                id="firstName"
+                name="Full Name"
+                className="w-full p-3 interFont text-[14px] text-slate-500 interestSectionBorder bg-slate-300 rounded-xl   focus:outline-none focus:ring-2 focus:ring-slate-300"
+                placeholder="Full Name"
+              />
+            </div>
 
-          <div className="mb-4">
-            <input
-              type="text"
-              id="lastName"
-              name="lastName"
-              className="w-full p-3 interFont text-[14px] text-slate-500 interestSectionBorder bg-slate-300 rounded-xl  focus:outline-none focus:ring-2 focus:ring-slate-300"
-              placeholder="Phone Number"
-            />
-          </div>
+            <div className="mb-4">
+              <input
+                type="text"
+                id="lastName"
+                name="lastName"
+                className="w-full p-3 interFont text-[14px] text-slate-500 interestSectionBorder bg-slate-300 rounded-xl  focus:outline-none focus:ring-2 focus:ring-slate-300"
+                placeholder="Phone Number"
+              />
+            </div>
 
-          <div className="mb-4">
-            <input
-              type="text"
-              id="email"
-              name="email"
-              className="w-full p-3 interFont text-[14px] text-slate-500 interestSectionBorder bg-slate-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-slate-300"
-              placeholder="Email"
-            />
-          </div>
-          <div className="mb-4 w-full">
-            <form className="max-w-sm mx-auto">
-              <select
-                id="countries"
-                className="bg-gray-50 SelectProjectBorder mainColor text-sm rounded-xl focus:ring-teal-400 focus:border-teal-400 block w-full p-2.5"
-              >
-                <option selected>Please Select a Project</option>
-                <option value="US">United States</option>
-                <option value="CA">Canada</option>
-                <option value="FR">France</option>
-                <option value="DE">Germany</option>
-              </select>
-            </form>
-          </div>
+            <div className="mb-4">
+              <input
+                type="text"
+                id="email"
+                name="email"
+                className="w-full p-3 interFont text-[14px] text-slate-500 interestSectionBorder bg-slate-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-slate-300"
+                placeholder="Email"
+              />
+            </div>
+            <div className="mb-4 w-full">
+              <form className="max-w-sm mx-auto">
+                <select
+                  id="countries"
+                  className="bg-gray-50 SelectProjectBorder mainColor text-sm   focus:outline-none rounded-xl focus:ring-teal-400 focus:border-teal-400 block w-full p-2.5"
+                >
+                  <option value="">Select a Project</option>
+                  {projectData.map((project) => (
+                    <option key={project.id} value={project.title}>
+                      {project.title}
+                    </option>
+                  ))}
+                </select>
+              </form>
+            </div>
 
-          <button
-            type="submit"
-            className="w-full  mainBackground text-white py-3 rounded-lg   focus:outline-none focus:ring-2 "
-          >
-            Register Your Interest
-          </button>
-        </form>
+            <button
+              type="submit"
+              className="w-full  mainBackground text-white py-3 rounded-lg   focus:outline-none focus:ring-2 "
+            >
+              Register Your Interest
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
